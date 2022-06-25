caf968cbd9edaf0ee3d9694df083d3619102374c
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-06-25 19:13:13 +0200

    Create config.js

---
+++ resource_server/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+    PORT: 4000,
+    introspectionEndpoint: "http://localhost:8080/realms/main/protocol/openid-connect/token/introspect",
+    userInfoEndpoint: "http://localhost:8080/realms/main/protocol/openid-connect/userinfo",
+    clientId: "webapp",
+    clientSecret: "qszmNquBzgL1lEws8A1DcgODqsD2PVGA",
+
+};
+
+export default config;
