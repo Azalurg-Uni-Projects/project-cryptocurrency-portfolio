2546cdbbda62c72e74beb0cd795c6d4649ba4fd7
Azalurg <patryk31415@gmail.com>
2022-06-29 10:16:28 +0200

    Name changing

--- server_side_app/config/config.js
+++ server_side_app/config/config.js
@@ -5,7 +5,7 @@ const config = {
     apiProtectedEndpoint: "http://localhost:4000/private",
     apiUnprotectedEndpoint: "http://localhost:4000/public",
     clientId: "webapp",
-    clientSecret: "qszmNquBzgL1lEws8A1DcgODqsD2PVGA",
+    clientSecret: "9draBjo5QWVMyh4IRaybWFi0pe7eTi9c",
     codeVerifier: "bd330d88a3899331934030a00007ac8d962d4169196b9d58a646b212",
     codeChallenge: "TKsCMm9IxqyWVyCNMZ3x-T4PTlSQyPNVPkR9NzOATp4",
     redirectUri: "http://localhost:5000/redirect"
